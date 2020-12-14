10d4c15007b4075731cc458eeaf076152bd790f3
ChirsLeeAreemm <ChengDongli997@163.com>
2020-12-14 15:25:55 +0800

    TableHelper:Add Create Table Helper
    Signed-off-by: ChirsLeeAreemm <ChengDongli997@163.com>

--- src/common/PlugsTableHelper.php
+++ src/common/PlugsTableHelper.php
@@ -7,15 +7,49 @@ namespace Siam\Plugs\common;
 
 
 use EasySwoole\Component\Singleton;
+use EasySwoole\DDL\DDLBuilder;
+use EasySwoole\DDL\Blueprint\Table;
+use EasySwoole\EasySwoole\Config;
+use EasySwoole\Mysqli\QueryBuilder;
+use EasySwoole\ORM\DbManager;
 
 class PlugsTableHelper
 {
     use Singleton;
 
-    // TODO 引用ddl组件  闭包执行完就执行mysql
-    function create($callable)
+    /**
+     *
+     * @param string $table 表名(不带前缀)
+     * @param mixed $callable DDL闭包
+     * @param string $connection 数据库连接(默认default)
+     * @throws \EasySwoole\ORM\Exception\Exception
+     * @throws \Throwable
+     * Author:chrisQx
+     */
+    function create(string $table, $callable)
     {
+        //检查前缀
+        $prefix     = Config::getInstance()->getConf('MYSQL.prefix');
+        $tableName  = "{$prefix}{$table}";
+        //检查表是否存在
+        $TableSql   = "SHOW TABLES LIKE '{$prefix}{$table}'";
+        $queryBuild = new QueryBuilder();
+        $queryBuild->raw($TableSql);
+        $checkTable = DbManager::getInstance()->query($queryBuild, true, 'default')->getResult();
+        if($checkTable){
+            throw new \Exception("表{$tableName}已存在");
+        }
 
+        //执行sql
+        $sql = DDLBuilder::table($tableName, function (Table $table) use ($callable){
+            $callable($table);
+        });
+        $queryBuild->raw($sql);
+        $create = DbManager::getInstance()->query($queryBuild, true, 'default')->getResult();//bool
+        if(!$create){
+            throw new \Exception("表{$tableName}创建失败");
+        }
+        return $create;
     }
 
     // TODO 删除表
@@ -23,4 +57,5 @@ class PlugsTableHelper
     {
 
     }
+
 }
