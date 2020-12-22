37c1d8384b93b0127469528ca2579c43147081b6
ChrisLee <ChengDongli997@163.com>
2020-12-22 18:03:28 +0800

    ServerHelper restart 已测试
    Signed-off-by: ChrisLee <ChengDongli997@163.com>

--- src/common/PlugsServerHelper.php
+++ src/common/PlugsServerHelper.php
@@ -2,7 +2,7 @@
 /**
  * 插件 server服务助手
  * User: Siam
- * Date: 2020/12/7 0007
+ * Date: 2020/12/7 0007e
  * Time: 21:28
  */
 
@@ -10,20 +10,29 @@ namespace Siam\Plugs\common;
 
 
 use EasySwoole\Component\Singleton;
+use EasySwoole\EasySwoole\Command\DefaultCommand\Server;
 
-class PlugsServerHelper
+
+class PlugsServerHelper extends Server
 {
 
     use Singleton;
 
+    /**
+     * 停止easyswoole服务
+     */
     public function stop()
     {
 
     }
 
+    /**
+     * 重启easyswoole服务
+     */
     public function restart()
     {
-
+        $path = EASYSWOOLE_ROOT;
+        exec("cd {$path} && ./restart_shell > {$path}/restart.log &");
     }
 
 
