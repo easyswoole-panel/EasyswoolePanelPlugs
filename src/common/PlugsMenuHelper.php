628f03f07d6c4a3a0f2af82abfed896a9c8acb63
ChrisLee <ChengDongli997@163.com>
2020-12-22 15:00:09 +0800

    MenuHelper
    Signed-off-by: ChrisLee <ChengDongli997@163.com>

--- src/common/PlugsMenuHelper.php
+++ src/common/PlugsMenuHelper.php
@@ -4,7 +4,10 @@
 namespace Siam\Plugs\common;
 
 
+use App\Model\AuthModel;
+use App\Model\SystemModel;
 use EasySwoole\Component\Singleton;
+
 class PlugsMenuHelper
 {
 
@@ -19,5 +22,26 @@ class PlugsMenuHelper
     public function add($menuName, $menuPath, $menuIcon)
     {
 
+        $data  = [
+            'auth_name'   => $menuName,
+            'auth_rules'  => $menuPath ?? '0',
+            'auth_icon'   => $menuIcon ?? '',
+            'auth_type'   => '0',
+            'create_time' => '0',
+            'update_time' => '0',
+        ];
+        $model = new AuthModel($data);
+        $rs = $model->save();
+        if ($rs) {
+            // 更新到排序中
+            $system = SystemModel::create()->get();
+            $auth = json_decode($system->auth_order);
+            $auth[] = ['id'=>$model->auth_id];
+            $system->auth_order = json_encode($auth);
+            $system->update();
+            return true;
+        } else {
+            throw new \Exception($model->lastQueryResult()->getLastError());
+        }
     }
 }
